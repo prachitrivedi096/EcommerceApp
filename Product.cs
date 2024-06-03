7ef490c16fa7ceda652d2d869e65a230c8d5e257
ptrivedi5448 <ptrivedi5448@conestogac.on.ca>
2024-06-03 09:05:18 -0400

    final commit

---
+++ Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceApp
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+
+        public Product(int productId, string productName, decimal price, int stock)
+        {
+            ProductID = productId;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+        public void IncreaseStock(int amount)
+        {
+            if (amount > 0)
+            {
+                Stock += amount;
+            }
+        }
+
+        public void DecreaseStock(int amount)
+        {
+            if (amount > 0 && Stock >= amount)
+            {
+                Stock -= amount;
+            }
+        }
+
+    }
+
+}
